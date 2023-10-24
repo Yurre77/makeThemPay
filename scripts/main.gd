08181454a65e762bdbf482a6ce341931a9442c44
Yurre77 <yurrr.schram@gmail.com>
2023-10-24 17:04:55 +0200

    added a couple functions

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,12 +4,35 @@ extends Node
 var score
 
 func _ready():
-	pass # Replace with function body.
+	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_score_timer_timeout():
+	score += 1
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio() = randf()
+
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	mob.position = mob_spawn_location.position
+
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	add_child(mob)
 
 func game_over():
 	$ScoreTimer.stop()
