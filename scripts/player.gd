441886ade22358e2ddb434e206811999b128417d
Yurre77 <yurrr.schram@gmail.com>
2023-10-24 13:52:30 +0200

    fixed an error

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,9 +19,9 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
@@ -33,9 +33,9 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$animatedSprite2D.play()
+		$AnimatedSprite2D.play()
 	else:
-		$animatedSprite2D.stop()
+		$AnimatedSprite2D.stop()
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
