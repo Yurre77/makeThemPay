e009aaa0cd85a2306a7e3c2c9d0b3cce5e639861
Yurre77 <yurrr.schram@gmail.com>
2023-10-24 16:57:46 +0200

    began coding the main scene

---
+++ scripts/main.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
