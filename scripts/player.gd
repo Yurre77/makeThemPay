90521b3d9f887e03957c0d7496d762144e5fa104
Yurre77 <yurrr.schram@gmail.com>
2023-10-26 13:53:15 +0200

    added movement code

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,21 @@ func _ready():
 	screen_size = get_viewport_rect().size()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var velocity = Vector2.ZERO
+	if(Input.is_action_pressed("move_right")):
+		velocity.x += 1
+	if(Input.is_action_pressed("move_left")):
+		velocity.x -= 1
+	if(Input.is_action_pressed("move_up")):
+		velocity.y -= 1
+	if(Input.is_action_pressed("move_down")):
+		velocity += 1
+
+	if(velocity.length() > 0):
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
 	pass
