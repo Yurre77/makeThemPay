58f704b01d671098c465181c6949e86ebaf5b3a5
Yurre77 <yurrr.schram@gmail.com>
2023-10-26 14:07:53 +0200

    added hit registration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
@@ -6,6 +7,7 @@ var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 func _process(delta):
@@ -37,3 +39,15 @@ func _process(delta):
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 	pass
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
