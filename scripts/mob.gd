7d7527ef311d8779df492712e89a29b5b20a209a
Yurre77 <yurrr.schram@gmail.com>
2023-10-26 14:23:13 +0200

    began coding the mob

---
+++ scripts/mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+func _ready():
+	$AnimatedSprite2D.play("Moving")
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
