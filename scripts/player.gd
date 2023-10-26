92a346c862ec9190917714d2b1ceb23f5c887f85
Yurre77 <yurrr.schram@gmail.com>
2023-10-26 14:10:02 +0200

    minor changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -37,6 +37,8 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "Up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+	else:
+		$AnimatedSprite2D.animation = "Idle"
 
 	pass
 
