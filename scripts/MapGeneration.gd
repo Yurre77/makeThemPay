941306830b4455f4d29221b235c3959171e39baa
Yurre <yurrr.schram@gmail.com>
2024-01-29 19:55:06 +0100

    began coding map generation (again)

---
+++ scripts/MapGeneration.gd
@@ -0,0 +1,8 @@
+extends TileMap
+
+@export var map_w:int = 80
+@export var map_h:int = 50
+@export var min_room_size:int = 8
+@export var min_room_factor:float = 0.4
+
+enum Tiles {GROUND, TREE, WATER, ROOF}
